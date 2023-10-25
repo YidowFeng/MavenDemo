0825b38598d18ed7fdfdf244df23031400c88321
YidowFeng <yidowfeng@gmail.com>
2023-10-25 18:31:01 -0400

    first commit

---
+++ src/main/java/org/example/day1/Day1Note.java
@@ -0,0 +1,19 @@
+package org.example.day1;
+
+public class Day1Note {
+    public static void main(String[] args) {
+        /*
+        1.maven
+            folder structure
+            types of repository: local(.m2), central, remote
+            maven build lifecycle
+            Command Line:
+                mvn clean, mvn test,...
+
+        2. git
+
+
+         */
+
+    }
+}
